7d50a85ce14a6da09143bb9c10312c3b98fc3a69
rcruz <rcruz@teleurban.mx>
2025-03-18 16:46:41 -0600

    feat(swift-auth): add SwiftAuth service provider and update configuration

--- src/Providers/SwiftAuthServiceProvider.php
+++ src/Providers/SwiftAuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Teleurban\SwiftAuth;
+namespace Teleurban\SwiftAuth\Providers;
 
 use Teleurban\SwiftAuth\Console\Commands\ExampleCommand;
 use Illuminate\Support\ServiceProvider;
@@ -9,22 +9,21 @@ final class SwiftAuthServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/swift-auth.php', 'swift-auth');
+        $this->mergeConfigFrom(__DIR__ . '/../config/swift-auth.php', 'swift-auth');
     }
 
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'swift-auth');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'swift-auth');
 
         $this->publishes(
             [
-                __DIR__ . '/../../resources/views' => resource_path('views/swift-auth'),
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/swift-auth'),
             ],
             [
-                'swift-auth:views',
-                'swift-auth',
+                'swift-auth:views'
             ]
         );
 
@@ -35,8 +34,7 @@ final class SwiftAuthServiceProvider extends ServiceProvider
                 __DIR__ . '/../database/migrations' => database_path('migrations'),
             ],
             [
-                'swift-auth:migrations',
-                'swift-auth'
+                'swift-auth:migrations'
             ]
         );
 
