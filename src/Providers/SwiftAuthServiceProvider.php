bde8202acc7fc2cddc49d4ae9b3ceb41c3821883
rcruz <rcruz@teleurban.mx>
2025-03-19 12:47:00 -0600

    fix(swift-auth): update paths for configuration, routes, views, and migrations in SwiftAuthServiceProvider

--- src/Providers/SwiftAuthServiceProvider.php
+++ src/Providers/SwiftAuthServiceProvider.php
@@ -10,29 +10,29 @@ final class SwiftAuthServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/swift-auth.php', 'swift-auth');
+        $this->mergeConfigFrom(__DIR__ . '/../../config/swift-auth.php', 'swift-auth');
     }
 
     public function boot(Router $router): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'swift-auth');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'swift-auth');
 
         $this->publishes(
             [
-                __DIR__ . '/../resources/views' => resource_path('views/vendor/swift-auth'),
+                __DIR__ . '/../../resources/views' => resource_path('views/vendor/swift-auth'),
             ],
             [
                 'swift-auth:views'
             ]
         );
 
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../migrations');
 
         $this->publishes(
             [
-                __DIR__ . '/../database/migrations' => database_path('migrations'),
+                __DIR__ . '/../../database/migrations' => database_path('migrations'),
             ],
             [
                 'swift-auth:migrations'
